ddb1b4da42309ee99ac86d1fed11183088d91e99
Tem <t3mir1an@gmail.com>
2023-03-09 23:46:47 -0600

    Initial Commit

---
+++ EthGasTracker/EthGasTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EthGasTrackerApp.swift
+//  EthGasTracker
+//
+//  Created by Tem on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct EthGasTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
