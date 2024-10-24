febddb22def73ec58a66a41d8d55a2a4f2c2551c
Tem <t3mir1an@gmail.com>
2024-10-23 23:02:01 -0500

    localize averages

--- EthGasTracker/Services/APIManager/Averages/AveragesResponse.swift
+++ EthGasTracker/Services/APIManager/Averages/AveragesResponse.swift
@@ -19,6 +19,7 @@ struct AverageEntry: Codable {
     let measureName: String
     let min: Double
     let minuteOfDay: Int
+    let minuteOfDayUTC: Int
     let p25: Double
     let p5: Double
     let p50: Double
@@ -31,7 +32,7 @@ struct AverageEntry: Codable {
         case max
         case measureName = "measure_name"
         case min
-        case minuteOfDay = "minute_of_day"
+        case minuteOfDayUTC = "minute_of_day"
         case p25
         case p5
         case p50
@@ -39,7 +40,7 @@ struct AverageEntry: Codable {
         case p95
     }
 
-    // Custom decoding to handle both String and Double for numeric fields
+    // Custom decoding to handle both String and Double
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
 
@@ -53,7 +54,9 @@ struct AverageEntry: Codable {
         p75 = try AverageEntry.decodeAsDouble(container, forKey: .p75)
         p95 = try AverageEntry.decodeAsDouble(container, forKey: .p95)
 
-        minuteOfDay = try AverageEntry.decodeAsInt(container, forKey: .minuteOfDay)
+        minuteOfDayUTC = try AverageEntry.decodeAsInt(container, forKey: .minuteOfDayUTC)
+        minuteOfDay = localizeMinuteOfDay(minuteOfDayUTC)
+        print("minuteOfDay: \(minuteOfDay) - minuteOfDayUTC: \(minuteOfDayUTC)")
         measureName = try container.decode(String.self, forKey: .measureName)
     }
 
@@ -67,8 +70,7 @@ struct AverageEntry: Codable {
         }
         throw DecodingError.typeMismatch(Double.self, DecodingError.Context(codingPath: container.codingPath, debugDescription: "Expected to decode Double or String but failed"))
     }
-
-    // Helper function to decode either an Int or String and convert it to Int
+    
     static func decodeAsInt(_ container: KeyedDecodingContainer<CodingKeys>, forKey key: CodingKeys) throws -> Int {
         if let intValue = try? container.decode(Int.self, forKey: key) {
             return intValue
@@ -78,4 +80,12 @@ struct AverageEntry: Codable {
         }
         throw DecodingError.typeMismatch(Int.self, DecodingError.Context(codingPath: container.codingPath, debugDescription: "Expected to decode Int or String but failed"))
     }
+}
+
+func localizeMinuteOfDay(_ minute: Int) -> Int {
+    let utcOffset = TimeZone.current.secondsFromGMT() / 60
+    let localMinute = (minute + utcOffset) % 1440
+
+    // If localMinute is negative, wrap it around to the end of the day
+    return localMinute >= 0 ? localMinute : localMinute + 1440
 }
